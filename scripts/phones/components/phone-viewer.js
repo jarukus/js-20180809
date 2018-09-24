b0b431b67abb1c592bc0c8cdcb54cfec77fb5f70
jarukus <jarik-vap@ukr.net>
2018-09-24 12:38:22 +0300

    add button 'back'
    add button 'back'

--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -14,14 +14,29 @@ export default class PhoneViewer extends Component {
     this._render();
 
     super.show();
+    this._element.addEventListener("click", event =>
+          this._buttonsClick(event)
+      );
   }
+
+    _buttonsClick(event) {
+        let phoneBack = event.target.closest('[data-element="phone-back"]');
+        let addBasket = event.target.closest('[data-element="add-basket"]');
+        if (phoneBack) {
+            document.querySelector('[data-component="phone-viewer"]').classList.add('js-hidden');
+            document.querySelector('[data-component="phone-catalog"]').classList.remove('js-hidden');
+        }
+        if (addBasket) {
+            alert("Товар добавлен в корзину");
+        }
+    }
 
   _render() {
     this._element.innerHTML = `
      <img class="phone" src="img/phones/motorola-xoom-with-wi-fi.0.jpg">
 
-    <button>Back</button>
-    <button>Add to basket</button>
+    <button data-element="phone-back">Back</button>
+    <button data-element="add-basket">Add to basket</button>
 
 
     <h1>${this._phone.name}</h1>
